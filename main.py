bda5527c64876109c7ef6e53c195703688248afd
DavidSoh[wsl2ubu] <mail@stdj.me>
2022-10-10 17:06:45 +0800

    prevent duplicate function name in future

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 # from pyStaticGen.post import convert
-from post import convert
+import post
 Path("mid_files").mkdir(parents=True,exist_ok=True)
 for post_path in Path("mid_files").glob('*'):
     Path(post_path).unlink()
-convert()
+post.convert()
